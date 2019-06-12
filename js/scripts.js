4e1277a0b193205535dafee83e3dffa7a802bfb0
Joseph Tubridy and Lisa Jensen <lisa@>
2019-06-12 16:08:21 -0700

    git nested for loops, still broken

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 $(function(){
   $("form").submit(function(event) {
 
-var vow =["a","e","i","o","u","A","E","I","O",'U'];
+var vow =["a","e","i","o","u","A","E","I","O",'U', undefined];
 var string =$("input#inputString").val();
 var outPutWords= [];
 var wordsArray = string.split(" ");
@@ -10,36 +10,38 @@ var wordsArray = string.split(" ");
       // var smooshWord =function()
       {
        // string =($("input#inputString").val());
-       alert(string);
-       alert(wordsArray);
+      // alert(string);
+       //alert(wordsArray);
        var lettersArray = wordsArray[0].split('');
        var chopped = [];
-       alert(lettersArray);
+       //alert(lettersArray);
        for (var index = 0; index < lettersArray.length; index++) {
          for(var i=0; i < vow.length; i++) {
-           if (lettersArray[index] === vow[i]) {
+           if (lettersArray[0] === vow[i]) {
             // index += lettersArray.length;
-            console.log("vowels")
+            console.log("first letter is a vowel");
+            break wordsArray[index].join('') + "way";
           } else {
             chopped.push(lettersArray.shift());
           }
           alert(chopped);
-
+        }
         if (chopped === undefined || chopped.length==0)
         {
           chopped.push("w");
         }
         var pigLatin = (lettersArray.join('') + chopped.join('') + "ay");
-        return pigLatin;
+        //return pigLatin;
 
       }
     }
     console.log(pigLatin)
-      // for ( var index = 0; index < lettersArray.length; index++){ outPutWords.push(smooshWord(string[index]));
-      //
-      // $("#output").append(outPutWords.join(' '))
+    //  for ( var index = 0; index < lettersArray.length; index++){ outPutWords.push(smooshWord(string[index]));
 
-    }
+      $("#output").append(outPutWords.join(' '))
+
+      event.preventDefault();
+    });
      //wordsArray.forEach(function())
 
     // for (var index = 0; index <= wordsArray.length; index += 1) {
@@ -53,7 +55,5 @@ var wordsArray = string.split(" ");
       //
       //   newStringArray[index]="-";
     // }
-    event.preventDefault();
   });
   // $("#output").text(outPutWords);
-});
